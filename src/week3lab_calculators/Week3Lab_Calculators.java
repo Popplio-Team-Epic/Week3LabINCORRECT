a050f55d044e0aea28900070b49580bb3726d5d0
Andrea <Andrea@Ezekiels-Laptop-Mark-Two.cg.shawcable.net>
2023-01-29 23:34:35 -0700

    New Project Creation Created a new Java Application for Week3Lab by Ezekiel/Evie Villaraza for CPRG 352 (Winter 2023).

---
+++ src/week3lab_calculators/Week3Lab_Calculators.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3lab_calculators;
+
+/**
+ *
+ * @author Andrea
+ */
+public class Week3Lab_Calculators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
